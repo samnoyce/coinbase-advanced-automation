d7980c4c36c10f6fa33d5f8e759b8cc960bd643e
samnoyce <contactnoyce@gmail.com>
2025-08-22 14:22:16 -0500

    Retrieve Coinbase API secret from AWS Secrets Manager

---
+++ internal/secret/coinbase.go
@@ -0,0 +1,57 @@
+package secret
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+)
+
+const (
+	versionStage  = "AWSCURRENT"
+	envRegion     = "AWS_REGION"
+	envSecretName = "COINBASE_SECRET_NAME"
+)
+
+type CoinbaseSecretResponse struct {
+	Name       string `json:"name"`
+	PrivateKey string `json:"privateKey"`
+}
+
+func GetCoinbaseSecret(ctx context.Context) (*CoinbaseSecretResponse, error) {
+	region, ok := os.LookupEnv(envRegion)
+	if !ok {
+		return nil, errors.New("secret: missing AWS_REGION environment variable")
+	}
+
+	secretName, ok := os.LookupEnv(envSecretName)
+	if !ok {
+		return nil, errors.New("secret: missing COINBASE_SECRET_NAME environment variable")
+	}
+
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(region))
+	if err != nil {
+		return nil, err
+	}
+
+	client := secretsmanager.NewFromConfig(cfg)
+
+	resp, err := client.GetSecretValue(ctx, &secretsmanager.GetSecretValueInput{
+		SecretId:     aws.String(secretName),
+		VersionStage: aws.String(versionStage),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var secret CoinbaseSecretResponse
+	if err := json.Unmarshal([]byte(*resp.SecretString), &secret); err != nil {
+		return nil, err
+	}
+
+	return &secret, nil
+}
